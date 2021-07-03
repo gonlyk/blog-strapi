5d6f90df41629d91bd1a8173d60de4b8172ad336
gonlyk <gonlyk@163.com>
2021-07-04 02:33:11 +0800

    添加指端

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', '47.243.111.6'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'strapi'),
         username: env('DATABASE_USERNAME', 'root'),
